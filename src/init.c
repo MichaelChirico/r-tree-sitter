cdb4ce368073f25828c48b7a0042739ef7b43b45
Michael Chirico <chiricom@google.com>
2026-06-21 17:58:32 -0700

    Mark R_init_treesitter visible

--- src/init.c
+++ src/init.c
@@ -1,4 +1,5 @@
 #include <R_ext/Rdynload.h>
+#include <R_ext/Visibility.h>
 #include <Rinternals.h>
 #include <stdbool.h>
 #include <stdlib.h>  // for NULL
@@ -229,7 +230,7 @@ static const R_CallMethodDef CallEntries[] = {
     {NULL, NULL, 0}
 };
 
-void R_init_treesitter(DllInfo* dll) {
+attribute_visible void R_init_treesitter(DllInfo* dll) {
   R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
   R_useDynamicSymbols(dll, FALSE);
 }
